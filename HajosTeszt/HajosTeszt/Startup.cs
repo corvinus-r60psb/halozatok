678b99f403d6dac5395d4f6f6c1a8820be9519c6
Balázs Urbán <balazs.urban@stud.uni-corvinus.hu>
2021-04-15 20:42:12 +0200

    API

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -16,6 +16,7 @@ namespace HajosTeszt
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,20 +26,26 @@ namespace HajosTeszt
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseHttpsRedirection();
 
             app.UseDefaultFiles();
-            app.UseStaticFiles();
+            app.UseStaticFiles(); //A sorrend fontos!
 
             app.UseRouting();
 
-           // app.UseEndpoints(endpoints =>
-           // {
-           //    endpoints.MapGet("/", async context =>
-           //     {
-           //        await context.Response.WriteAsync("Hello World!");
-           //    });
-          //  });
-       }
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Hello World!");
+            //    });
+            //});
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
     }
 }
