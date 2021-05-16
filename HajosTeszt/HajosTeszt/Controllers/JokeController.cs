6c6a9339a5c46cbcb0d982084e7d90b1925dfc2c
Balázs Urbán <balazs.urban@stud.uni-corvinus.hu>
2021-05-16 19:38:23 +0200

    Crude

--- HajosTeszt/HajosTeszt/Controllers/JokeController.cs
+++ HajosTeszt/HajosTeszt/Controllers/JokeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HajosTeszt.JokeModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,52 +7,55 @@ using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace HajosTeszt.Controllers
+namespace EmptyBoat.Controllers
 {
     [Route("api/jokes")]
     [ApiController]
     public class JokeController : ControllerBase
     {
-
-        public static List<Joke> Jokes = new List<Joke>();
-
-        // GET: api/<JokeController>
+        // GET: api/jokes
         [HttpGet]
-        public ActionResult Get()
+        public IEnumerable<Joke> Get()
         {
-            return new JsonResult(Jokes);
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return context.Jokes.ToList();
         }
 
-        // GET api/<JokeController>/5
         [HttpGet("{id}")]
-        public ActionResult Get(int id)
+        public Joke Get(int id)
         {
-            return new JsonResult(Jokes[id]);
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var keresettVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            return keresettVicc;
         }
 
         // POST api/<JokeController>
         [HttpPost]
-        public void Post([FromBody] Joke joke)
+        public void Post([FromBody] Joke újVicc)
         {
-            Jokes.Add(joke);
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
         }
 
         // PUT api/<JokeController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] Joke joke)
+        public void Put(int id, [FromBody] string value)
         {
-            Jokes[id] = joke;
         }
 
         // DELETE api/<JokeController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
-            Jokes.RemoveAt(id);
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendővicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendővicc);
+            context.SaveChanges();
         }
-    }
-    public class Joke
-    {
-        public string Text { get; set; }
     }
 }
